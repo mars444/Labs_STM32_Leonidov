03155d2c4cc0490cc19d4a2d08ac2fbedb38ad76
PetrPamuzhak <petromars444@gmail.com>
2021-10-19 20:20:33 +0300

    Реализован вывод цифр от 0 до 9 по кнопке  с помощью 9 циклов))

--- laba_1_asm.s
+++ laba_1_asm.s
@@ -10,103 +10,165 @@ mov32	r0, PERIPH_BB_BASE+ \
 
 
 
-	MOV32	R0, GPIOA_CRL
 
+	MOV32	R0, GPIOA_CRL
     MOV32	R1, #0x33333333
 	LDR		R2, [R0]
     BFI		R2, R1, #0, #31
     STR		R2, [R0]
-					@ загрузить результат в регистр настройки порта
-
- 			@ адрес порта выходных сигналов
-
-			@ загружаем в порт
 
 
 	MOV32	R0, GPIOA_CRH
-
-    MOV32	R1, #0x3
+    MOV32	R1, #0x333
 	LDR		R2, [R0]
     BFI		R2, R1, #0, #4
     STR		R2, [R0]
 
+    MOV32	R0, GPIOA_CRH
+    MOV32	R1, #0x88
+	LDR		R2, [R0]
+    BFI		R2, R1, #4, #8
+    STR		R2, [R0]
 
   	mov32	r0, GPIOA_BSRR
-		@ адрес порта выходных сигналов
-
-
-
-
-loop:									@ Бесконечный цикл
-	mov32 	r1, #0x1F2				@ устанавливаем вывод в '1'
-	str 	r1, [r0]				@ загружаем в пор
-	bl		delay						@ задержка
-	bl      Reset_number
-
-
-	mov32 	r1, #0xC0				@ устанавливаем вывод в '1'
-	str 	r1, [r0]					@ загружаем в порт
-	bl		delay						@ задержка
-	bl      Reset_number
-
-
-	mov32 	r1, #0x1B1				@ устанавливаем вывод в '1'
-	str 	r1, [r0]				@ загружаем в порт
-	bl		delay						@ задержка
-	bl      Reset_number
-
-
-	mov32 	r1, #0x1E1				@ устанавливаем вывод в '1'
+	mov32 	r1, #0x1F2
 	str 	r1, [r0]
-	bl		delay						@ задержка
-	bl      Reset_number
 
-	mov32 	r1, #0xC3				@ устанавливаем вывод в '1'
+loop1:
+	push	{r2,r3}
+ 	MOV32	R3, GPIOA_IDR
+	LDR		R2, [R3]
+	AND     R2, #0x200
+	CMP     R2, #0
+	pop		{r2,r3}
+	IT      EQ
+	BEQ     loop1
+	Bl Reset_number
+	bl delay
+	mov32 	r1, #0xC0				@ устанавливаем вывод в '0'
 	str 	r1, [r0]
-	bl		delay						@ задержка
-	bl      Reset_number
 
-	mov32 	r1, #0x163				@ устанавливаем вывод в '1'
+
+loop2:
+	push	{r2,r3}
+ 	MOV32	R3, GPIOA_IDR
+	LDR		R2, [R3]
+	AND     R2, #0x200
+	CMP     R2, #0
+	pop		{r2,r3}
+	IT      EQ
+	BEQ     loop2
+	Bl Reset_number
+	bl delay
+	mov32 	r1, #0x1B1				@ устанавливаем вывод в '0'
 	str 	r1, [r0]
-	bl		delay						@ задержка
-	bl      Reset_number
 
-	mov32 	r1, #0x173				@ устанавливаем вывод в '1'
+
+loop3:
+	push	{r2,r3}
+ 	MOV32	R3, GPIOA_IDR
+	LDR		R2, [R3]
+	AND     R2, #0x200
+	CMP     R2, #0
+	pop		{r2,r3}
+	IT      EQ
+	BEQ     loop3
+	Bl Reset_number
+	bl delay
+	mov32 	r1, #0x1E1				@ устанавливаем вывод в '0'
 	str 	r1, [r0]
-	bl		delay						@ задержка
-	bl      Reset_number
 
-	mov32 	r1, #0x1C0				@ устанавливаем вывод в '1'
+
+loop4:
+	push	{r2,r3}
+ 	MOV32	R3, GPIOA_IDR
+	LDR		R2, [R3]
+	AND     R2, #0x200
+	CMP     R2, #0
+	pop		{r2,r3}
+	IT      EQ
+	BEQ     loop4
+	Bl Reset_number
+	bl delay
+	mov32 	r1, #0xC3				@ устанавливаем вывод в '0'
 	str 	r1, [r0]
-	bl		delay						@ задержка
-	bl      Reset_number
 
-	mov32 	r1, #0x1F3				@ устанавливаем вывод в '1'
+
+loop5:
+	push	{r2,r3}
+ 	MOV32	R3, GPIOA_IDR
+	LDR		R2, [R3]
+	AND     R2, #0x200
+	CMP     R2, #0
+	pop		{r2,r3}
+	IT      EQ
+	BEQ     loop5
+	Bl Reset_number
+	bl delay
+	mov32 	r1, #0x163				@ устанавливаем вывод в '0'
 	str 	r1, [r0]
-	bl		delay						@ задержка
-	bl      Reset_number
 
 
-	mov32 	r1, #0xF1				@ устанавливаем вывод в '1'
+loop6:
+	push	{r2,r3}
+ 	MOV32	R3, GPIOA_IDR
+	LDR		R2, [R3]
+	AND     R2, #0x200
+	CMP     R2, #0
+	pop		{r2,r3}
+	IT      EQ
+	BEQ     loop6
+	Bl Reset_number
+	bl delay
+	mov32 	r1, #0x173				@ устанавливаем вывод в '0'
 	str 	r1, [r0]
-	bl		delay						@ задержка
-	bl      Reset_number
 
 
-	b 		loop						@ возвращаемся к началу цикла
+loop7:
+	push	{r2,r3}
+ 	MOV32	R3, GPIOA_IDR
+	LDR		R2, [R3]
+	AND     R2, #0x200
+	CMP     R2, #0
+	pop		{r2,r3}
+	IT      EQ
+	BEQ     loop7
+	Bl Reset_number
+	bl delay
+	mov32 	r1, #0x1C0				@ устанавливаем вывод в '0'
+	str 	r1, [r0]
 
-delay:									@ Подпрограмма задержки
-	push	{r0}						@ Загружаем в стек R0, т.к. его значение будем менять
-	mov32	r0, 0xFFFFF					@ псевдоинструкция Thumb (загрузить константу в регистр)
-delay_loop:
-	subs	r0, #1						@ SUB с установкой флагов результата
-	it 		NE
-	bne		delay_loop					@ переход, если Z==0 (результат вычитания не равен нулю)
-	pop		{r0}						@ Выгружаем из стека R0
-	bx     LR
+
+loop8:
+	push	{r2,r3}
+ 	MOV32	R3, GPIOA_IDR
+	LDR		R2, [R3]
+	AND     R2, #0x200
+	CMP     R2, #0
+	pop		{r2,r3}
+	IT      EQ
+	BEQ     loop8
+	Bl Reset_number
+	bl delay
+	mov32 	r1, #0x1F3				@ устанавливаем вывод в '0'
+	str 	r1, [r0]
 
 
-	Reset_number:
+loop9:
+	push	{r2,r3}
+ 	MOV32	R3, GPIOA_IDR
+	LDR		R2, [R3]
+	AND     R2, #0x200
+	CMP     R2, #0
+	pop		{r2,r3}
+	IT      EQ
+	BEQ     loop9
+	Bl Reset_number
+	bl delay
+	mov32 	r1, #0x1E3				@ устанавливаем вывод в '0'
+	str 	r1, [r0]
+
+Reset_number:
 	mov32	r1, #GPIO_BSRR_BR5			@ сбрасываем в '0'
 	str 	r1, [r0]
 	mov32 	r1, #GPIO_BSRR_BR0					@ устанавливаем вывод в '1'
@@ -124,5 +186,19 @@ delay_loop:
 	mov32 	r1, #GPIO_BSRR_BR9					@ устанавливаем вывод в '1'
 	str 	r1, [r0]
 	bx      lr
+
+delay:									@ Подпрограмма задержки
+	push	{r0}						@ Загружаем в стек R0, т.к. его значение будем менять
+	mov32	r0, 0xFFFFF					@ псевдоинструкция Thumb (загрузить константу в регистр)
+delay_loop:
+	subs	r0, #1						@ SUB с установкой флагов результата
+	it 		NE
+	bne		delay_loop					@ переход, если Z==0 (результат вычитания не равен нулю)
+	pop		{r0}						@ Выгружаем из стека R0
+	bx     LR
+
+
+
+
 
 //end code  //end code  //end code //end code  //end code
